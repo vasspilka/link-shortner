dc4f45fe27e6b6c0cd3523618d166a10fa665cd5
Vasilis Spilka <vasspilka@gmail.com>
2021-07-13 08:43:39 +0200

    add react and tailwind

---
+++ assets/js/react/app.js
@@ -0,0 +1,4 @@
+import { define } from "remount";
+import Home from "./components/Home";
+
+define({ 'x-home': Home });
---
+++ assets/js/react/components/Home.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Home = () => <h1 className="text-2xl text-center"> Wellcome to Short URL </h1>;
+
+export default Home;
